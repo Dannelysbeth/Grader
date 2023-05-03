3b7c1099b6f27c8ecb8a167f1f81cd22e6d5c6df
danifoj315 <danifoj315@student.polsl.pl>
2023-05-03 23:39:42 +0200

    completed stage 4/7 of project Grader

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -5,32 +5,39 @@ import java.util.Scanner;
 public class Main {
     static int cows = 0;
     static int bulls = 0;
-
     static String secretKey = null;
 
-    final static int CODE_LENGTH = 4;
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-//        grader(secretKey.toCharArray(), scanner.next().toCharArray());
-//        if (cows == 0 && bulls == 0) {
-//            System.out.printf("Grade: None. The secret code is %s.\n", secretKey);
-//        } else {
-//            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.\n", bulls, cows, secretKey);
-//        }
+
+        System.out.println("Please, enter the secret code's length:");
         int digitNum = scanner.nextInt();
         if (digitNum > 10) {
             System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
         } else {
             generateRandomNumber(digitNum);
-            System.out.printf("The random secret number is %s.\n", secretKey);
+            System.out.println("Okay, let's start a game! ");
+            int turn = 1;
+            while (bulls != digitNum) {
+                System.out.printf("Turn %d:\n", turn);
+                grader(secretKey.toCharArray(), scanner.next().toCharArray());
+                if (cows == 0 && bulls == 0) {
+                    System.out.println("Grade: None");
+                } else {
+                    System.out.printf("Grade: %d bull(s) and %d cow(s).\n", bulls, cows);
+                }
+                turn++;
+            }
+            System.out.println("Congratulations! You guessed the secret code.");
         }
 
 
     }
 
     public static void grader(char[] secretCode, char[] userCode) {
+        cows = 0;
+        bulls = 0;
         for (int i = 0; i < secretCode.length; i++) {
             for (int j = 0; j < userCode.length; j++) {
                 if (secretCode[i] == userCode[j]) {
@@ -49,7 +56,7 @@ public class Main {
             String pseudoRandomNumber = String.valueOf(System.nanoTime());
             if (secretKey == null) {
                 int i = pseudoRandomNumber.length() - 1;
-                while(Character.getNumericValue(pseudoRandomNumber.charAt(i)) == 0) {
+                while (Character.getNumericValue(pseudoRandomNumber.charAt(i)) == 0) {
                     i--;
                 }
                 secretKey = String.valueOf(pseudoRandomNumber.charAt(i));
